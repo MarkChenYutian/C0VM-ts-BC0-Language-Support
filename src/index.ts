7acec2724f21b7c0fe21eb7022cbe5bfe0594d2f
Yutian Chen <yutianch@andrew.cmu.edu>
2022-06-10 21:36:11 -0400

    :package: BC0 Syntax Parser Generator Configuration based on Lezer

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,22 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const BC0Language = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
       styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
+        Byte: t.atom,
+        LineComment: t.comment,
+        FuncHeader: t.className,
+        JMPLabel: t.moduleKeyword
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: {line: "# "}
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function BC0() {
+  return new LanguageSupport(BC0Language)
 }
